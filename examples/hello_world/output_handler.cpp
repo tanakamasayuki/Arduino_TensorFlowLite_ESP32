c6a88286b0ec8a3a99810704eb49240409a9b541
root <akiraneko@gmail.com>
2020-05-26 22:58:55 +0900

    Add base 2.1.1

---
+++ examples/hello_world/output_handler.cpp
@@ -0,0 +1,59 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ==============================================================================*/
+
+#include "output_handler.h"
+
+#include "Arduino.h"
+#include "constants.h"
+
+// The pin of the Arduino's built-in LED
+#if defined(LED_BUILTIN)
+int led = LED_BUILTIN;
+#elif defined(ARDUINO_M5Stick_C)
+int led = 10;
+#else
+int led = -1;
+#endif
+
+// Track whether the function has run at least once
+bool initialized = false;
+
+// Animates a dot across the screen to represent the current x and y values
+void HandleOutput(tflite::ErrorReporter* error_reporter, float x_value,
+                  float y_value) {
+  // Do this only once
+  if (!initialized) {
+    // Set the LED pin to output
+    if (led != -1) {
+      pinMode(led, OUTPUT);
+      ledcSetup(0, 1200, 8);
+      ledcAttachPin(led, 0);
+    }
+    initialized = true;
+  }
+
+  // Calculate the brightness of the LED such that y=-1 is fully off
+  // and y=1 is fully on. The LED's brightness can range from 0-255.
+  int brightness = (int)(127.5f * (y_value + 1));
+
+  // Set the brightness of the LED. If the specified pin does not support PWM,
+  // this will result in the LED being on when y > 127, off otherwise.
+  if (led != -1) {
+    ledcWrite(0, brightness);
+  }
+
+  // Log the current brightness value for display in the Arduino plotter
+  error_reporter->Report("%d\n", brightness);
+}
